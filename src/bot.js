84bbdb5ad8a53b58b11992e12bd761314377f298
= <=>
2016-12-29 17:31:31 -0500

    Rps game is nearly done, credits to brian x for inspiration

--- src/bot.js
+++ src/bot.js
@@ -4,20 +4,44 @@ const bot = new Discord.Client();
 const token = 'MjY0MTIwNDYyMjQwNTE0MDUw.C0b82w.Dlcjk52uyiKmBLXlCpucxEToFdI';
 
 //Variables
-let pf = "#"
+let pf = "%"
+let info = "**INFO > **"
+
 //Bot Initiation
 bot.login(token);
 
+//Functions
+function randNum (min, max) {
+    return Math.floor(Math.random * (max - min ) + min)
+}
+function rps (userInput) {
+    let botChoice = randNum(1,3)
+    user = user.toUpperCase
+
+    switch (bot) {
+    case 1:
+        bot = "ROCK"
+        break
+    case 2:
+        bot = "PAPER"
+        break
+    case 3:
+        bot = "SCISSORS"
+        break
+    }
+    if (user === "ROCK") {
+        if (bot === "ROCK") return ["LOLOL IT WAS A TIE XD, You played: " + user + " , the bot played " + bot "."]
+    }
+}
+
+//Message Interactions
 bot.on('ready', () => {
-    console.log('I am ready!');
+    console.log('Bot is online!');
     bot.user.setGame("v0.0.1 - By Jason L.")
 });
 
 bot.on('message', message => {
     if(message.content.startsWith(pf + 'ding')) {
-        message.channel.sendMessage('dong');
+        message.channel.sendMessage(info + 'Dong!');
     }
-    if(message.content.startsWith(pf + 'rps') {
-        
-    })
 });
