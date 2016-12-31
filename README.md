d1f0df3acc05d1f7a647e9e5f812d013027ae2cb
= <=>
2016-12-30 20:14:05 -0500

    timer and rps function implemented, can be used through chat commands, v0.0.4 achieved

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Dank_Bot
 
 Dank_Bot is a simple Discord bot that will have many different functions. Currently still in development.
-
-Created by Jason Liu
+Bot Image is from <html><a href = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Video-Game-Controller-Icon-IDV-green.svg/1024px-Video-Game-Controller-Icon-IDV-green.svg.png
+Created by Jason Liu">here</a></html>
--- src/bot.js
+++ src/bot.js
@@ -5,19 +5,22 @@ const token = 'MjY0MTIwNDYyMjQwNTE0MDUw.C0b82w.Dlcjk52uyiKmBLXlCpucxEToFdI';
 
 //Variables
 let pf = "%";
-let info = "**INFO > **";
-let version = 'v0.0.3'
+let info = "**:exclamation: INFO > **";
+let timer = "**:alarm_clock: TIMER > **"
+let version = 'v0.0.4'
 
 //Bot Initiation
 bot.login(token);
 
 //Functions
+//Random number generator, generates number between two numbers, inclusive
 function randNum (min, max) {
     return Math.floor(Math.random * (max - min ) + min)
 }
+//Rock Paper Scissors function, works as you would expect a rock paper scissors game to work
 function rps (userInput) {
     let botChoice = randNum(1,3)
-    user = user.toUpperCase
+    user = userInput.toUpperCase
 
     switch (bot) {
     case 1:
@@ -47,7 +50,12 @@ function rps (userInput) {
         if (bot === "SCISSORS") return ["LOLOL IT WAS A TIE XD" + rpsSay]
     }
 }
-
+//Timer function, takes time in seconds and something to remind the user of
+function timer (time, toRemind) {
+    setTimeout(function() {
+        message.channel.sendMessage(timer + "You have to " + toRemind + message.member.user + "!");
+    }, (time * 1000));
+}
 //Message Interactions
 bot.on('ready', () => {
     console.log('Bot is online!');
@@ -55,14 +63,24 @@ bot.on('ready', () => {
 });
 
 bot.on('message', message => {
+    //Ding dong message to test
     if(message.content.startsWith(pf + 'ding')) {
         message.channel.sendMessage(info + 'Dong!');
     }
+    if(message.content.startsWith(pf + 'rps')) {
+        let userChoice = message.content.split(' ')
+        if (userChoice[1].toUpperCase === "ROCK" || userChoice[1].toUpperCase === "PAPER" || userChoice[1].toUpperCase === "SCISSORS") {
+            let results = rps(userChoice[1]);
+            message.channel.sendMessage(results);
+        }
+    }
+    //Kills the bot
     if(message.content.startsWith(pf + 'kill')) {
         message.channel.sendMessage(info + 'Drinking Bleach...');
         bot.destroy()
     }
-    if(message.content.includes('roblox' || 'Roblox' || 'ROBLOX')) {
+    //If roblox is in a message, bot replies accordingly
+    if(message.content.toUpperCase.includes('ROBLOX')) {
         message.channel.sendMessage(info + 'Ew, are you six years old?');
     }
 });
